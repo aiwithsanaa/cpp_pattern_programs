0eee3f18faa69d3c6a7768bff490e01408eb2665
Sanaa <khansanaa418@gmail.com>
2026-07-14 16:59:39 +0530

    Add pyramid,hollow triangle and hollow diamond patterns

---
+++ README.md
@@ -0,0 +1,18 @@
+# C++ Pattern Programs
+
+This repository contains beginner-friendly C++ pattern programs.
+
+## Programs Included
+- Butterfly Pattern
+- Heart Pattern
+- Hollow Diamond Pattern
+- Hollow Triangle Pattern
+- Pyramid Pattern
+- Square Pattern
+- Triangle Pattern
+
+## Language
+- C++
+
+## Author
+Sanaa
---
+++ hollowtriangle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++)
+        {
+            if(j==0||j==(2*i)||i==n-1)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
